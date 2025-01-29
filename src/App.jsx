edd2888f4b03c23613bec961a7fe5e8749d4c5ea
ADRIAN LINARES <adrianlinares246@gmail.com>
2025-01-28 22:07:38 -0500

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./routes/components/NavBar";
 import { HomeScreen } from "./routes/HomeScreen";
+import { LoginScreen } from "./routes/LoginScreen";
 import { AboutScreen } from "./routes/AboutScreen";
 import { ContactScreen } from "./routes/ContactScreen";
+import { UserProvider } from "./routes/context/UserProvider";
 
 export const App = () => {
     return (
-        <>
+        <UserProvider>
             <NavBar></NavBar>
 
             <Routes>
                 <Route path="/" element={<HomeScreen></HomeScreen>}></Route>
+                <Route
+                    path="/login"
+                    element={<LoginScreen></LoginScreen>}
+                ></Route>
                 <Route
                     path="/about"
                     element={<AboutScreen></AboutScreen>}
@@ -21,6 +27,6 @@ export const App = () => {
                 ></Route>
                 <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
             </Routes>
-        </>
+        </UserProvider>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-        <StrictMode>
+        <React.StrictMode>
             <App />
-        </StrictMode>
+        </React.StrictMode>
     </BrowserRouter>
 );
